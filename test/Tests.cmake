a7bcc0080e6b640d5dbc32fde420f006a4467f22
Fahien <sir.fahien@gmail.com>
2018-06-23 12:53:07 +0200

    Link Android library

--- test/Tests.cmake
+++ test/Tests.cmake
@@ -6,7 +6,6 @@ function(make_test TEST_NAME)
 	# Executable
 	add_executable(${TEST_NAME} ${TEST_SOURCES})
 	target_include_directories(${TEST_NAME} PUBLIC ${LST_INCLUDE_DIRS})
-	set_target_properties(${TEST_NAME} PROPERTIES LINKER_LANGUAGE CXX)
 	target_link_libraries(${TEST_NAME} ${LST_LIBRARIES})
 	add_test(${TEST_NAME} ${EXECUTABLE_OUTPUT_PATH}/${TEST_NAME})
 endfunction(make_test)
